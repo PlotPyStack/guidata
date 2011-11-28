77b2406a94383d4585b0706e4c3957ed1818fbed
Pierre Raybaut <pierre.raybaut@cea.fr>
2011-11-28 12:24:33 +0100

    build_chm.bat: added support for Windows x64

--- build_chm.bat
+++ build_chm.bat
@@ -1,4 +1,6 @@
 sphinx-build -b htmlhelp doc doctmp
 "C:\Program Files\HTML Help Workshop\hhc.exe" doctmp\guidatadoc.hhp
+"C:\Program Files (x86)\HTML Help Workshop\hhc.exe" doctmp\guidatadoc.hhp
 copy doctmp\guidatadoc.chm .
 rmdir /S /Q doctmp
+pause
