db31d71067444dc2a26e4f10f993c8ad3482a0e8
Pierre Raybaut <pierre.raybaut@gmail.com>
2015-10-07 19:00:55 +0200

    Doc: added link to CHM documentation

--- build_chm.bat
+++ build_chm.bat
@@ -1,6 +1,13 @@
 sphinx-build -b htmlhelp doc doctmp
-"C:\Program Files\HTML Help Workshop\hhc.exe" doctmp\guidatadoc.hhp
-"C:\Program Files (x86)\HTML Help Workshop\hhc.exe" doctmp\guidatadoc.hhp
-copy doctmp\guidatadoc.chm .
+"C:\Program Files\HTML Help Workshop\hhc.exe" doctmp\guidata.hhp
+"C:\Program Files (x86)\HTML Help Workshop\hhc.exe" doctmp\guidata.hhp
+copy doctmp\guidata.chm .
+7z a guidata.chm.zip guidata.chm
+del doctmp\guidata.chm
+del doc.zip
+sphinx-build -b html doc doctmp
+cd doctmp
+7z a -r ..\doc.zip *.*
+cd ..
 rmdir /S /Q doctmp
-pause
+del guidata.chm.zip
