489d60417ccb8d6a51d05e972ab666d06bd6f91f
Pierre Raybaut <pierre.raybaut@gmail.com>
2015-12-01 12:07:04 +0100

    Added CHM documentation to wheel package

--- setup.py
+++ setup.py
@@ -25,6 +25,11 @@ from __future__ import print_function
 import setuptools  # analysis:ignore
 from distutils.core import setup
 import sys
+import os
+import os.path as osp
+import shutil
+import atexit
+import subprocess
 
 from guidata.utils import get_subpackages, get_package_data
 
@@ -45,11 +50,47 @@ else:
     CLASSIFIERS += ['Development Status :: 5 - Production/Stable']
 
 
+def build_chm_doc(libname):
+    """Return CHM documentation file (on Windows only), which is copied under 
+    {PythonInstallDir}\Doc, hence allowing Spyder to add an entry for opening 
+    package documentation in "Help" menu. This has no effect on a source 
+    distribution."""
+    args = ''.join(sys.argv)
+    if os.name == 'nt' and ('bdist' in args or 'build' in args):
+        try:
+            import sphinx  # analysis:ignore
+        except ImportError:
+            print('Warning: `sphinx` is required to build documentation',
+                  file=sys.stderr)
+            return
+        hhc_base = r'C:\Program Files%s\HTML Help Workshop\hhc.exe'
+        for hhc_exe in (hhc_base % '', hhc_base % ' (x86)'):
+            if osp.isfile(hhc_exe):
+                break
+        else:
+            print('Warning: `HTML Help Workshop` is required to build CHM '\
+                  'documentation file', file=sys.stderr)
+            return
+        doctmp_dir = 'doctmp'
+        subprocess.call('sphinx-build -b htmlhelp doc %s' % doctmp_dir,
+                        shell=True)
+        atexit.register(shutil.rmtree, osp.abspath(doctmp_dir))
+        fname = osp.abspath(osp.join(doctmp_dir, '%s.chm' % libname))
+        subprocess.call('"%s" %s' % (hhc_exe, fname), shell=True)
+        if osp.isfile(fname):
+            return fname
+        else:
+            print('Warning: CHM building process failed', file=sys.stderr)
+
+CHM_DOC = build_chm_doc(LIBNAME)
+
+
 setup(name=LIBNAME, version=version,
       description=DESCRIPTION, long_description=LONG_DESCRIPTION,
       packages=get_subpackages(LIBNAME),
       package_data={LIBNAME:
                     get_package_data(LIBNAME, ('.png', '.svg', '.mo'))},
+      data_files=[(r'Doc', [CHM_DOC])] if CHM_DOC else [],
       entry_points={'gui_scripts':
                     ['guidata-tests-py%d = guidata.tests:run'\
                      % sys.version_info.major,]},
