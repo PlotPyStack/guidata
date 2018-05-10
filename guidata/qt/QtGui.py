78bc0956f0b250f38c04488ba835c059a05d2463
stonebig <stonebig>
2018-05-10 22:10:44 +0200

    hammer patch on guidata "qt"
    dwarf proverb: if you believe that violence does not solve anything, it
    is that you do not hit hard enough

--- guidata/qt/QtGui.py
+++ guidata/qt/QtGui.py
@@ -4,17 +4,5 @@
 # Licensed under the terms of the MIT License
 # (copied from Spyder source code [spyderlib.qt])
 
-import os
-
-if os.environ['QT_API'] == 'pyqt5':
-    from PyQt5.QtCore import QSortFilterProxyModel            # analysis:ignore
-    from PyQt5.QtPrintSupport import (QPrinter, QPrintDialog, # analysis:ignore
-                                      QAbstractPrintDialog)
-    from PyQt5.QtPrintSupport import QPrintPreviewDialog      # analysis:ignore
-    from PyQt5.QtGui import *                                 # analysis:ignore
-    from PyQt5.QtWidgets import *                             # analysis:ignore
-elif os.environ['QT_API'] == 'pyqt':
-    from PyQt4.Qt import QKeySequence, QTextCursor            # analysis:ignore
-    from PyQt4.QtGui import *                                 # analysis:ignore
-else:
-    from PySide.QtGui import *                                # analysis:ignore
+from ..qtpy.QtGui import *                                 # analysis:ignore
+from ..qtpy.QtWidgets import *                           
--- guidata/qt/QtSvg.py
+++ guidata/qt/QtSvg.py
@@ -4,11 +4,4 @@
 # Licensed under the terms of the MIT License
 # (see spyderlib/__init__.py for details)
 
-import os
-
-if os.environ['QT_API'] == 'pyqt5':
-    from PyQt5.QtSvg import *                                 # analysis:ignore
-elif os.environ['QT_API'] == 'pyqt':
-    from PyQt4.QtSvg import *                                 # analysis:ignore
-else:
-    from PySide.QtSvg import *                                # analysis:ignore
+from ..qtpy.QtSvg import *  
