4f802c3a9ee95321c0745daf5a7e4b0f10bffd04
Pierre Raybaut <pierre.raybaut@gmail.com>
2020-08-19 16:07:35 +0200

    Minor dist/doc/tests-related changes

--- build_dist.bat
+++ build_dist.bat
@@ -1,7 +1,5 @@
 del MANIFEST
 rmdir /S /Q build
 rmdir /S /Q dist
-python setup.py build bdist_wininst --plat-name=win32
-python setup.py build bdist_wininst --plat-name=win-amd64
 python setup.py sdist bdist_wheel --universal
 python setup.py build sdist
