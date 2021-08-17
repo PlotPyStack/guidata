93f0c7699f917b73759058bdc581d8a2df267e59
Pierre Raybaut <pierre.raybaut@gmail.com>
2021-08-17 08:37:20 +0200

    Fixed upload.bat script

--- upload.bat
+++ upload.bat
@@ -1,5 +1,5 @@
 rmdir /S /Q build
 rmdir /S /Q dist
 python setup.py build sdist upload
-python setup.py sdist bdist_wheel --universal upload
+python setup.py build bdist_wheel --universal upload
 pause
