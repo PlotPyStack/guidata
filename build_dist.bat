71546b649a5add21d1a3924e4616ac0467b009e8
Pierre Raybaut <pierre.raybaut@gmail.com>
2022-11-15 10:34:15 +0100

    build_dist.bat: removed --universal (no Py2!)

--- build_dist.bat
+++ build_dist.bat
@@ -1,5 +1,5 @@
 del MANIFEST
 rmdir /S /Q build
 rmdir /S /Q dist
-python setup.py sdist bdist_wheel --universal
+python setup.py sdist bdist_wheel
 python setup.py build sdist
--- build_doc.bat
+++ build_doc.bat
@@ -2,12 +2,12 @@ sphinx-build -b htmlhelp doc doctmp
 "C:\Program Files\HTML Help Workshop\hhc.exe" doctmp\guidata.hhp
 "C:\Program Files (x86)\HTML Help Workshop\hhc.exe" doctmp\guidata.hhp
 copy doctmp\guidata.chm .
-7z a guidata.chm.zip guidata.chm
+"C:\Program Files\7-Zip\7z.exe" a guidata.chm.zip guidata.chm
 del doctmp\guidata.chm
 del doc.zip
 sphinx-build -b html doc doctmp
 cd doctmp
-7z a -r ..\doc.zip *.*
+"C:\Program Files\7-Zip\7z.exe" a -r ..\doc.zip *.*
 cd ..
 rmdir /S /Q doctmp
 del guidata.chm.zip
