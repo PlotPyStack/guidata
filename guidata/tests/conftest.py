90d13d0a1620661fd7a46e158e78575f03caf3be
Pierre Raybaut <p.raybaut@codra.fr>
2024-03-12 14:32:56 +0100

    pytest: moved conftest.py to project root

--- guidata/tests/conftest.py
+++
@@ -1,7 +0,0 @@
-# content of conftest.py
-
-from guidata.env import execenv
-
-# Turn on unattended mode for executing tests without user interaction
-execenv.unattended = True
-execenv.verbose = "quiet"
