0732c9b421bc6b75100bffeb049fb688d2120a1e
Pierre Raybaut <p.raybaut@codra.fr>
2024-06-13 11:35:02 +0200

    Update conftest.py to include additional information in pytest report header

--- conftest.py
+++ conftest.py
@@ -1,7 +1,21 @@
 # content of conftest.py
 
+import qtpy
+
+import guidata
 from guidata.env import execenv
 
 # Turn on unattended mode for executing tests without user interaction
 execenv.unattended = True
 execenv.verbose = "quiet"
+
+
+def pytest_report_header(config):
+    """Add additional information to the pytest report header."""
+    qtbindings_version = qtpy.PYSIDE_VERSION
+    if qtbindings_version is None:
+        qtbindings_version = qtpy.PYQT_VERSION
+    return [
+        f"guidata {guidata.__version__}, "
+        f"{qtpy.API_NAME} {qtbindings_version} [Qt version: {qtpy.QT_VERSION}]",
+    ]
