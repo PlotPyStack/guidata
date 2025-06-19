56e32e98eca484d710e54cc76d9e2b0328120241
Pierre Raybaut <p.raybaut@codra.fr>
2025-06-19 17:11:37 +0200

    Refactor pytest task in tasks.json and update conftest.py for improved configuration options

--- guidata/tests/conftest.py
+++ guidata/tests/conftest.py
@@ -1,5 +1,7 @@
 # content of conftest.py
 
+import os
+
 import qtpy
 
 import guidata
@@ -8,6 +10,22 @@ from guidata.env import execenv
 # Turn on unattended mode for executing tests without user interaction
 execenv.unattended = True
 execenv.verbose = "quiet"
+
+
+def pytest_addoption(parser):
+    """Add custom command line options to pytest."""
+    parser.addoption(
+        "--show-windows",
+        action="store_true",
+        default=False,
+        help="Display Qt windows during tests (disables QT_QPA_PLATFORM=offscreen)",
+    )
+
+
+def pytest_configure(config):
+    """Configure pytest based on command line options."""
+    if not config.getoption("--show-windows"):
+        os.environ.setdefault("QT_QPA_PLATFORM", "offscreen")
 
 
 def pytest_report_header(config):
