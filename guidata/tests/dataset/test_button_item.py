3561a2cbac54877145c0fcdd4d679eebfc79be40
Pierre Raybaut <p.raybaut@codra.fr>
2025-08-25 18:22:28 +0200

    Add test for button item functionality in GUI context

---
+++ guidata/tests/dataset/test_button_item.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the terms of the BSD 3-Clause
+# (see guidata/LICENSE for details)
+
+"""
+Button item test
+
+This item is tested separately from other items because it is special: contrary to
+other items, it is purely GUI oriented and has no sense in a non-GUI context.
+"""
+
+# guitest: show
+
+import os.path as osp
+import re
+
+from qtpy import QtCore as QC
+from qtpy import QtWidgets as QW
+
+import guidata.dataset as gds
+from guidata.env import execenv
+from guidata.qthelpers import exec_dialog, qt_app_context
+
+
+def information_selectable(parent: QW.QWidget, title: str, text: str) -> int:
+    """Show an information message box with selectable text."""
+    box = QW.QMessageBox(parent)
+    box.setIcon(QW.QMessageBox.Information)
+    box.setWindowTitle(title)
+    if re.search(r"<[a-zA-Z/][^>]*>", text):
+        box.setTextFormat(QC.Qt.RichText)
+        box.setTextInteractionFlags(QC.Qt.TextBrowserInteraction)
+    else:
+        box.setTextFormat(QC.Qt.PlainText)
+        box.setTextInteractionFlags(
+            QC.Qt.TextSelectableByMouse | QC.Qt.TextSelectableByKeyboard
+        )
+    box.setText(text)
+    box.setStandardButtons(QW.QMessageBox.Ok)
+    return exec_dialog(box)
+
+
+class Parameters(gds.DataSet):
+    """
+    DataSet test
+    The following text is the DataSet 'comment': <br>Plain text or
+    <b>rich text<sup>2</sup></b> are both supported,
+    as well as special characters (α, β, γ, δ, ...)
+    """
+
+    def button_callback(dataset, item, value, parent):
+        execenv.print(f"Button clicked: {dataset}, {item}, {value}, {parent}")
+        text = "<br>".join(
+            [
+                f"<b>Dataset</b>: {'<br>' + '<br>'.join(str(dataset).splitlines())}",
+                f"<b>Item</b>: {item}",
+                f"<b>Value</b>: {value}",
+            ]
+        )
+        information_selectable(parent, "Button Clicked", text)
+
+    dir = gds.DirectoryItem("Directory", osp.dirname(__file__))
+    preview = gds.TextItem("File names preview")
+    button = gds.ButtonItem("Click me", button_callback, "MessageBoxInformation")
+
+
+def test_button_item():
+    """Test button item"""
+    with qt_app_context():
+        prm = Parameters()
+        execenv.print(prm)
+        if prm.edit():
+            execenv.print(prm)
+
+
+if __name__ == "__main__":
+    test_button_item()
